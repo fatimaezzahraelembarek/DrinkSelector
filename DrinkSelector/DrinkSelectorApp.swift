25e2554a6ee95e7e308cfa63436fc98a4b89f983
fatimaezzahraelembarek <fatimaezzahraelembarek21@gmail.com>
2024-12-02 09:58:56 +0100

    Initial Commit

---
+++ DrinkSelector/DrinkSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkSelectorApp.swift
+//  DrinkSelector
+//
+//  Created by Fatima Ezzahrae LEMBAREK on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DrinkSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
