47a0578ee17b968fe89b467dee4b2ccc22241395
fatimaezzahraelembarek <fatimaezzahraelembarek21@gmail.com>
2024-12-02 10:03:12 +0100

    xcode project updated

--- DrinkSelectorTests/DrinkSelectorTests.swift
+++ DrinkSelectorTests/DrinkSelectorTests.swift
@@ -14,7 +14,7 @@ final class DrinkSelectorTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
+   
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
