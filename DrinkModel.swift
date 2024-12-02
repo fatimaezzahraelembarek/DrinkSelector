4175f9675901b51df3d99b753d566a822a9f5eb0
fatimaezzahraelembarek <fatimaezzahraelembarek21@gmail.com>
2024-12-02 11:58:28 +0100

    app changes : for the first ecran

---
+++ DrinkModel.swift
@@ -0,0 +1,8 @@
+//
+//  DrinkModel.swift
+//  DrinkSelector
+//
+//  Created by Fatima Ezzahrae LEMBAREK on 02/12/2024.
+//
+
+import Foundation
---
+++ OrderSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderSummaryView.swift
+//  DrinkSelector
+//
+//  Created by Fatima Ezzahrae LEMBAREK on 02/12/2024.
+//
+
+import SwiftUI
+
+struct OrderSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderSummaryView()
+}
